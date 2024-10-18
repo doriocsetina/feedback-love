8e027e9e2a62fc147ad4d24051272adcdd1f3c37
doriocsetina <dariogjergo@proton.me>
2024-10-18 14:05:25 +0200

    implemented walk on tiles

--- scenes/player.gd
+++ scenes/player.gd
@@ -2,35 +2,33 @@ extends CharacterBody2D
 
 var direction : Vector2 = Vector2.ZERO
 const tile_size = Vector2(64, 32)
-var target_position = Vector2()
+var selected_tile = Vector2()
 
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 func _onready():
-	target_position = position
+	global_position = selected_tile
+	pass
 	
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-		var click_position = event.position
 		var tile_map_layer = Main.get_tile_map_layer()
-		print("i clicked the mouse")
+		print("i clicked the mouse at ", event.position)
 		if tile_map_layer:
-			var tile_pos = tile_map_layer.local_to_map(click_position)
-			var world_pos = tile_map_layer.map_to_local(tile_pos) + tile_size / 2
-			print("there is a maplayetr")
-			target_position = world_pos
-			print("it's adjacent")
+			var tile_pos = tile_map_layer.local_to_map(get_global_mouse_position())
+			selected_tile = tile_map_layer.map_to_local(tile_pos)
+			print("going to: ", selected_tile)
 
-func _physics_process(delta: float) -> void:
-	var direction = (target_position - position).normalized() 
-	position = position.lerp(target_position, 0.1)
-	if direction:
+func _physics_process(_delta: float) -> void:
+	direction = selected_tile - global_position 
+	var distance = direction.length()
+
+	if distance > 1:
 		set_walking(true)
+		global_position = global_position.lerp(selected_tile, 0.1)
 		update_blend_position()
 	else:
 		set_walking(false)
-		self.velocity = Vector2.ZERO
-		
 
 # Check if the tile is adjacent and valid
 func is_adjacent_and_valid(tile_pos: Vector2) -> bool:
--- scenes/tile_map_layer.gd
+++ scenes/tile_map_layer.gd
@@ -1,9 +1,10 @@
 extends TileMapLayer
 
-
+var selectedTile
+ 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Main.set_tile_map_layer(self)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
